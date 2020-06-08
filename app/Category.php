af31c2fbbde5c4ef95b5d9a609ea9e3f92744532
Tomas <tomas@vilbar.lt>
2020-06-08 09:04:53 +0300

    Define model relationships

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The products that belong to the category.
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'category_products');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The categories that product belongs to.
+     */
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_products');
+    }
 }
